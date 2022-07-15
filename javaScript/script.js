7ad6ab70001b1dbe7543dbe213bd28dc4effb08e
jesusp301081 <jesusp301081@gmail.com>
2022-07-15 12:17:35 -0500

    upload my portfolio

---
+++ javaScript/script.js
@@ -0,0 +1,12 @@
+let darkmode_logo = document.getElementById('darkmode_logo');
+
+darkmode_logo.addEventListener('click', () => {
+    alert('hola');
+});
+
+let win = window;
+
+win.addEventListener('scroll', () =>{
+    
+});
+
